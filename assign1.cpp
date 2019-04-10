d957f57bedd4c72815c10c5319a07a9afbe341c7
David B <dbaugh.oregon@gmail.com>
2019-04-10 10:41:41 -0700

    Added txt files and graph-search pseudocode jpg

--- assign1.cpp
+++ assign1.cpp
@@ -1,1 +1,16 @@
+#include<iostream>  
 
+//COMPILE : g++ -o assign1 assign1.cpp
+//RUN     : assign1 startState.txt goalState.txt --MODE-- finishedState.txt
+
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main (int argc, char *argv[]) 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+}
