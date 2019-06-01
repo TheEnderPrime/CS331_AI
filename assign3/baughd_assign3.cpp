741348c14997aa7b9208f6cbf26ec6cc5a72cf2a
David B <dbaugh.oregon@gmail.com>
2019-06-01 09:16:06 -0700

    added beginning code and training sets

--- assign3/baughd_assign3.cpp
+++ assign3/baughd_assign3.cpp
@@ -1,8 +1,16 @@
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <queue>
+#include <stdlib.h>
 using namespace std;
 
+//COMPILE : g++ -o assign3 baughd_assign3.cpp 
+//COMPILE ADD ON MY DESKTOP : -static-libstdc++ -static-libgcc
 
-String removePunctuationFromFile(char *file)
+vector<string> removePunctuationFromFile(char *file)
 {
     fstream startFile;
 
@@ -12,24 +20,32 @@ String removePunctuationFromFile(char *file)
     string line;
     while (getline(startFile, line))
     {
-        for (int i = 0, len = str.size(); i < len; i++)
+        for (int i = 0, len = line.size(); i < len; i++)
         {
             // check whether parsing character is punctuation or not
             if (ispunct(line[i]))
             {
                 line.erase(i--, 1);
-                len = str.size();
+                len = line.size();
             }
+            line[i] = tolower(line[i]);
         }
-        results.push_back
+        results.push_back(line);
     }
-    return state;
+    return results;
 }
 
-int main()
+vector<string> createVocab(vector<string> rawWords)
 {
-   
+    
+}
 
+int main(int argc, char *argv[])
+{
+    cout << "Assignment 3 - David Baugh" << endl;
+    vector<string> trainingSet = removePunctuationFromFile(argv[1]);
+    vector<string> testingSet = removePunctuationFromFile(argv[2]);
+    cout << trainingSet[0] << endl;
 
 
 
