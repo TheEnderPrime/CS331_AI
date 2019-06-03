e6567a5a22190b9ec8802f38878dc297edcc946a
David B <dbaugh.oregon@gmail.com>
2019-06-02 19:36:00 -0700

    mostly working preprocessing

--- assign3/baughd_assign3.cpp
+++ assign3/baughd_assign3.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 #include <algorithm>
 #include <stdlib.h>
+#include <iterator>
 using namespace std;
 
 //COMPILE : g++ -o assign3 baughd_assign3.cpp
@@ -36,6 +37,34 @@ vector<string> removePunctuationAndCaseFromFile(char *file)
     return results;
 }
 
+vector<string> getClassLabels(vector<string> data)
+{
+    vector<string> results;
+    for (int i = 0; i < data.size(); i++)
+    {
+        vector<string> temp;
+        istringstream ss(data[i]);
+
+        // Traverse through all words
+        do
+        {
+            // Read a word
+            string word;
+            ss >> word;
+
+            // Print the read word
+            if (isdigit(word[0]))
+            {
+                temp.push_back(word);
+            }
+
+            // While there is more to read
+        } while (ss);
+        results.push_back(temp.back());
+    }
+    return results;
+}
+
 vector<string> createVocab(vector<string> rawWords)
 {
     vector<string> words;
@@ -51,7 +80,7 @@ vector<string> createVocab(vector<string> rawWords)
             ss >> word;
 
             // Print the read word
-            if (!isdigit(word[0]) && (find(words.begin(), words.end(), word) == words.end() || words.size() == 0))
+            if (!isdigit(word[0]) && (find(words.begin(), words.end(), word) == words.end() || words.size() == 0) && word != "\t" && word != " " && word != "")
             {
                 words.push_back(word);
             }
@@ -63,34 +92,157 @@ vector<string> createVocab(vector<string> rawWords)
     return words;
 }
 
-vector<string> convertIntoFeatures(vector<string> data, vector<string> vocab) {
+vector<vector<string>> convertIntoFeatures(vector<string> data, vector<string> vocab, vector<string> classLabels)
+{
     int M = vocab.size();
-    vector<vector<int>> features;
-
-    for(int i = 0; i < data.size(); i++){
-        vector<int> feature;
+    vector<vector<string>> features;
+    features.push_back(vocab);
+    for (int i = 0; i < data.size(); i++)
+    {
+        vector<string> feature;
+        string value;
         features.push_back(feature);
-        for(int j=0 ; j < vocab.size(); j++) {
-            if()
+        for (int j = 0; j < vocab.size(); j++)
+        {
+            string vocabWord = vocab[j];
+            int size = data[i].size();
+            //cout << data[i][size-2] << endl;
+            if (data[i].find(vocabWord) == vocabWord[0])
             {
-                feature
-                features[i].push_back()
+                cout << "convert: YES to 1. Found -" << vocabWord << "- in " << data[i] << endl;
+                value = "1";
+            } else
+            {
+                value = "0";
             }
+            features[i].push_back(value);
         }
+        features[i].push_back(classLabels[i]);
+    }
+
+    return features;
+}
+
+void outputToFile(vector<vector<string>> preprocessedVector, string fileName)
+{
+    ofstream output_file(fileName);
+    ostream_iterator<string> output_iterator(output_file, ",");
+    for (int i = 0; i < preprocessedVector.size(); i++)
+    {
+        copy(preprocessedVector.at(i).begin(), preprocessedVector.at(i).end(), output_iterator);
+        output_file << '\n';
     }
 }
+
+
+
+// # assign probability of good/bad vocab
+// def assignVocabProb(vocab, training, truth):
+//     tRecords = 0 # num of trues ( good )
+//     fRecords = 0 # num of false ( Bad )
+//     for value in truth:
+//         if value == 1:
+//             tRecords += 1
+//         else:
+//             fRecords += 1
+//     result = {}
+//     for index, entry in enumerate(vocab):
+//         ttCount = 0 # true/true
+//         ftCount = 0 # false/true
+//         ffCount = 0 # false/false
+//         tfCount = 0 # true/false
+//         for sen in training:
+//             if sen[index] == 1:
+//                 if sen[-1] == 1:
+//                     ttCount += 1
+//                 else:
+//                     tfCount += 1
+//             else:
+//                 if sen[-1] == 1:
+//                     ftCount += 1
+//                 else:
+//                     ffCount += 1
+//         probs = (ttCount / tRecords, ftCount/tRecords, tfCount/fRecords, ffCount/fRecords)
+//         result[entry] = probs
+//     return result, (tRecords/len(truth), fRecords/len(truth)) # result array and prob
+
+// #
+// def Test(sentences, trained, probs, vocab):
+//     res = []
+//     probTrue = probs[0]
+//     probFalse = probs[1]
+//     for sentence in sentences:
+//         probT = probTrue
+//         # go through every word and check to see if its in given trained data for both t/f
+//         for index, word in enumerate(sentence):
+//             if vocab[index] in trained:
+//                 if word == 1:
+//                     probT *= trained[vocab[index]][0]
+//                 else:
+//                     probT *= trained[vocab[index]][1]
+//         probF = probFalse
+//         for index, word in enumerate(sentence):
+//             if vocab[index] in trained:
+//                 if word == 1:
+//                     probF *= trained[vocab[index]][2]
+//                 else:
+//                     probF *= trained[vocab[index]][3]
+//         if probT >= probF:
+//             res.append(1)
+//         else:
+//             res.append(0)
+//     return res
+
+// # compares accuracy of pos or neg review with real value
+// def checkAccuracy(calc, real):
+//     correct = 0
+//     for i in range(len(real)):
+//         if calc[i] == real[i]:
+//             correct += 1
+//     return correct / len(real)
+
 
 int main(int argc, char *argv[])
 {
     cout << "Assignment 3 - David Baugh" << endl;
+    cout << "Get Data" << endl;
     vector<string> trainingSet = removePunctuationAndCaseFromFile(argv[1]);
     vector<string> testingSet = removePunctuationAndCaseFromFile(argv[2]);
-    
+
+    cout << "Pre-Processing" << endl;
+    vector<string> classLabels = getClassLabels(trainingSet);
     vector<string> trainingVocab = createVocab(trainingSet);
 
+    vector<vector<string>> preprocessed_train = convertIntoFeatures(trainingSet, trainingVocab, classLabels);
+    vector<vector<string>> preprocessed_test = convertIntoFeatures(testingSet, trainingVocab, classLabels);
 
-    for( int i = 0; i < trainingVocab.size(); i++){
-        cout << trainingVocab[i] << endl;
+    outputToFile(preprocessed_train, "preprocessed_train.txt");
+    outputToFile(preprocessed_test, "preprocessed_test.txt");
+
+    // print("training...\n")
+    // trainVocab, (p1, p2) = assignVocabProb(vocab, training, trainTruth)
+    
+    // print("Classifing...\n")
+    // trainClassification = Test(training, trainVocab, (p1, p2), vocab)
+    // testClassification = Test(testing, trainVocab,(p1, p2), vocab)
+
+    // print("Checking accuracy...\n")
+    // res1 = checkAccuracy(trainClassification, trainTruth)
+    // res2 = checkAccuracy(testClassification, testTruth)
+
+
+    int test = 0;
+    for (int i = 0; i < preprocessed_train.size(); i++)
+    {
+        for (int j = 0; j < preprocessed_train[i].size(); j++)
+        {
+            if (preprocessed_train[i][j] == "1")
+            {
+                test++;
+            }
+            //cout << preprocessed_train[i][j] << endl;
+        }
     }
+    cout << "test: " << test << endl;
     return 0;
 }
